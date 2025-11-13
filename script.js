b08d360999c9b01909f142ce6820f18483b3bc70
carlo milan noble <cgmnstudio@outlook.com>
2025-11-13 11:55:04 -0500

    updated gallery

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
          'Picture2-3.png',
 	     'SightseeStampDesignFR2-14.png',
 	     'SightseeStampDesignFR-13.png',
+         'DnMSept7.png'
     ];
 
     const gallery = document.getElementById('gallery');
